725f3a1793bbe8d3c3f486123ea14cd77a5a3fab
Roger <r.holden@chi.ac.uk>
2024-10-09 14:58:49 +0100

    initial commit

---
+++ src/Adam.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Adam {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+
+        Vehicle vehicle = new Vehicle("Toyota");
+//        System.out.println( vehicle.getClass().toString());
+        vehicle.moveit();
+        System.out.println("Your car is a "+vehicle.manufacturer);
+
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,16 @@
+public class Vehicle {
+
+    public String manufacturer;
+
+    public Vehicle(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void move(){
+
+        System.out.println("brmm brmm");
+    }
+
+    public void moveit() {
+    }
+}
