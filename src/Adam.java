8fa434cfb2b5707c4c59e5ec8a12da52306c5ae8
Roger <r.holden@chi.ac.uk>
2024-10-16 15:00:23 +0100

    test for HTTPS access

--- src/Adam.java
+++ src/Adam.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// test comment for git test
 public class Adam {
     public static void main(String[] args) {
         System.out.println("Hi");
